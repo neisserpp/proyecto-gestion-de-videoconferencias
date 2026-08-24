c034590b065bbcf7aaf3643331aca011172de4fb
neisserpp <neffex.pp@gmail.com>
2026-08-23 19:42:58 -1000

    Some Change

--- reserva_vc/settings.py
+++ reserva_vc/settings.py
@@ -130,10 +130,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'app' / 'static',
-]
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
